4287eda0718ab52d0f23d2aec930dbdbf4956c48
WhateverNO <9530nooo@gmail.com>
2017-08-15 13:54:23 +0530

    Added draw Functions

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-var c = document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
-ctx.beginPath();
-var x=20,r=10,y=15;
-for(var i =0;i<5;i++)
-    {   ctx.moveTo(x+r,y+r);
-        ctx.arc(x,y+r,r,0,2*Math.PI);
+var c = document.getElementById("toolkit");
+var ctx = c.getContext("2d");
+
+window.onload = function() { // This functions whenever the page is loaded
+    Draw() ;
+}
+
+function Draw() { // A normal function which inclues complete draw method
+    ctx.beginPath();
+    var x , y , radius ;
+    x = 40 ;
+    y = 20 ;
+    radius = 10 ;
+    
+    for(var i = 0 ; i < 5 ; i++) {   
+        ctx.moveTo(x+radius , y+radius);
+        ctx.arc(x  , y+radius , radius , 0 , 2*Math.PI);
         ctx.stroke();
         x+=45;
     }
+}
 
