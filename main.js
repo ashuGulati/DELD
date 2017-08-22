c071f29d12cb3800e5102668b8947a32ab446aa8
ashuGulati <ashugulati1104@gmail.com>
2017-08-22 15:59:55 +0530

    rectangle click

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 var c = document.getElementById("toolkit");
 var ctx = c.getContext("2d");
 
@@ -23,6 +24,7 @@ function Draw() { // A normal function which inclues complete draw method
     ctx.lineTo(407,80);
     ctx.stroke();
     x=40,y=100;
+    
     for(var i=0;i<5;i++){  //for rectangles
         ctx.rect(x,y,30,30);
         ctx.stroke();
@@ -43,35 +45,84 @@ function Draw() { // A normal function which inclues complete draw method
     }
     
 
- function Button(x,y,w,h){
+ 
      
      
-     var inside = isInside() ;
-//      var toplx=x;
-//      var toply=y;
-//      var toprx=x+w;
-//      var topry=y;
-//      var botlx=x;
-//      var botly=y-h;
-//      var botrx=x+w;
-//      var botry=y-h;
-     var topLeft = new Vector(x,y);
-     var bottomRight = new Vector(x+w,y-h);
-     var topRight = new Vector(x+w,y-h);
-     var bottomRight =new Vector(x+w,y-h);
-  } 
-function Rectangle(x,y,w,h){
-        this.x=x;
-        this.y=y;
-        this.w=w;
-        this.h=h;
-    }   
-var posi =new Rectangle()
-     
-       
- function isInside(pos,rect){
-        if  ((pos.x > rect.x) && (pos.x < (rect.x+rect.w){
-              if(( pos.y < (rect.y+rect.h) && (pos.y > rect.y))
-        return true;
-              }
+
+    // var topLeft = new Vector(40,100);
+     //var bottomRight = new Vector(x+w,y-h);
+     //var topRight = new Vector(x+w,y-h);
+     //var bottomRight =new Vector(x+w,y-h);
+//     var inside = isInside() ;
+
+
+function mousePos(event){//when we click on ccanvas this functuion is called
+   
+    var x = event.clientX ;
+    var y = event.clientY ;
+    console.log(x+" , "+y) ;
+    
+    var pos = new Vector(x,y) ;
+    
+     if(isInside(pos, rect)){
+       console.log("clicked inside"); 
+    }
+    else{
+        console.log("clicked outside");                
+    } 
+}
+
+    function isInside( pos, rect){//to check mouse pos and rectangle pos
+        if  ( (pos.x > rect.x) && (pos.x < (rect.x+rect.w)) )
+              if(( pos.y < (rect.y+rect.h) && (pos.y > rect.y)))
+                return true;
+              
         }
+
+    function Rectangle(x,y,w,h){
+    this.x=x;
+    this.y=y;
+    this.w=w;
+    this.h=h;
+    
+}
+ var arr=[]; 
+var x1=40;
+ for(i=0;i<5;i++){
+     
+     var rect = new Rectangle(x1,100,30,30);
+     arr.push(rect);
+     x1+=45;
+      
+ }
+     c.addEventListener("click" , mousePos) ;
+
+
+  function Button(){
+      this.rect= new Rectangle(30,40,30,30);
+      this.color= "#FF0000";
+      
+  }      
+  Button.prototype.Draw=function(ctx){
+     ctx.rect(this.rect.x,this.rect.y,this.rect.w,this.rect.h);
+        
+  }
+     
+    
+
+var myButton=[];
+for(var i=0;i<5;i++){
+    var butn = new Button(30,40,30,30);
+    myButton.push(butn);
+    
+}
+
+    for (var i=0;i<myButton.length;i++){
+        myButton[i].Draw(ctx);
+    }
+        
+       
+
+
+
+
